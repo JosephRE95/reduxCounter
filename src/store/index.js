6a58919f431f234a46ebf305c4406ecba55d898e
Joseph Ellis <josephellis95@gmail.com>
2024-01-21 18:17:32 -0600

    Provder and store created from redux

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+// import { connect } from "react-redux";
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "add") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "sub") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
